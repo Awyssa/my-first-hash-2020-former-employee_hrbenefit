fb60d925067e0f37ab3bd7e9700ec73cc7ce1e1a
Michael Ernest <m.hendersun@gmail.com>
2020-10-06 00:02:10 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -29,7 +29,6 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3,
                  }
-               end
   shipping_manifest["muskets"] => 2
   shipping_manifest["gun powder"] => 4
 
